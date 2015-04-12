01d061032b5b9f9ad3034e4988cb6b89d8ed7939
Naoki Nishida <domitry@gmail.com>
2015-04-12 18:26:26 +0900

    Move plot_raw_mri to CategorizedMRI#plot

--- ipysurfer/mri.py
+++ ipysurfer/mri.py
@@ -170,3 +170,38 @@ class CategorizedMRI(__MRI__):
 
         self.data = arr
         self.label = rule_alive
+
+    def plot(self, filter=True, config={}):
+        from jinja2 import Template
+        from IPython.core.display import display, HTML
+        from tempfile import TemporaryFile
+        from base64 import b64encode
+        from uuid import uuid4
+
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), "template/vis.html"))
+        template = Template(open(path).read())
+
+        # encode png as base64
+        f = TemporaryFile("r+b")
+        self.to_png(f, filter=filter)
+        f.seek(0)
+        png = "data:image/png;base64," + b64encode(f.read())
+        size = {
+            "width": int(self.width),
+            "height": int(self.height),
+            "depth": int(self.depth),
+            "frames_per_row": int(sqrt(self.depth)),
+            "frames_per_column": int(sqrt(self.depth))
+        }
+        config.update({"voltex_size": size})
+
+        label_rules = [[v[0], map(int, list(v[1]))] for k, v in self.label.items()]
+
+        html = template.render(**{
+            "div_id": "vis" + str(uuid4()),
+            "encoded_png": png,
+            "label" : label_rules,
+            "config": config
+        })
+
+        display(HTML(html))
